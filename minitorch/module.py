0e5639f0ffd92cb417d2f9ec5248b5e21f42c4e7
105969161+atgctg@users.noreply.github.com <105969161+atgctg@users.noreply.github.com>
2022-06-07 19:21:00 +0200

    module (task 4)

--- minitorch/module.py
+++ minitorch/module.py
@@ -21,13 +21,23 @@ class Module:
 
     def train(self):
         "Set the mode of this module and all descendent modules to `train`."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+
+        def _train(module):
+            module.training = True
+            for m in module.modules():
+                _train(m)
+
+        _train(self)
 
     def eval(self):
         "Set the mode of this module and all descendent modules to `eval`."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+
+        def _eval(module):
+            module.training = False
+            for m in module.modules():
+                _eval(m)
+
+        _eval(self)
 
     def named_parameters(self):
         """
@@ -37,13 +47,25 @@ class Module:
         Returns:
             list of pairs: Contains the name and :class:`Parameter` of each ancestor parameter.
         """
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+
+        def _named_parameters(module, prefix=""):
+            for name, param in module._parameters.items():
+                yield prefix + name, param
+            for name, module in module._modules.items():
+                yield from _named_parameters(module, prefix + name + ".")
+
+        return list(_named_parameters(self))
 
     def parameters(self):
         "Enumerate over all the parameters of this module and its descendents."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+
+        # def _parameters(module):
+        #     for param in module._parameters.values():
+        #         yield param
+        #     for module in module._modules.values():
+        #         yield from _parameters(module)
+
+        return [param for _, param in self.named_parameters()]
 
     def add_parameter(self, k, v):
         """
